a210ec99b80a7eddc5b54f2ea701af605e59ac19
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-02 21:24:39 +0200

    Connecting with supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://hqqtbuqiwtuetrmwwbia.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
     </Row>
   );
 }
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hqqtbuqiwtuetrmwwbia.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxcXRidXFpd3R1ZXRybXd3YmlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyOTY1NjIsImV4cCI6MjA0MDg3MjU2Mn0.DWfn0D75yZPqpQCvKHdC9Al284VqLyKH35TPCFOwXr0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
