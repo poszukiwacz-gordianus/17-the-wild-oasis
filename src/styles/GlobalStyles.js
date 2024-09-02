0a6b5753a032fa50b905f2d7c4c6dedc41d2a99f
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-02 13:58:29 +0200

    Global Styles with styled components

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,15 +1,6 @@
-/*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
-*/
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
 
 /* Colors adapted from https://tailwindcss.com/docs/customizing-colors */
 
@@ -194,3 +185,6 @@ FOR DARK MODE
 --image-grayscale: 10%;
 --image-opacity: 90%;
 */
+`;
+
+export default GlobalStyles;
