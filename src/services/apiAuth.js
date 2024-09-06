f07b79d8495e3c3a2b9d60d7d70b1a40db34f37d
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-06 19:58:18 +0200

    Add user logout

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -16,8 +16,12 @@ export async function getCurrentUser() {
   if (!session.session) return null;
 
   const { data, error } = await supabase.auth.getUser();
-  console.log(data);
   if (error) throw new Error(error.message);
 
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
